6c7ffe1c0de19926ce7d462d19d547b02095f4f6
Snowsdy <shadowss.yt@gmail.com>
2022-11-10 19:26:48 +0100

    initial commit

---
+++ config/variables.php
@@ -0,0 +1,3 @@
+<?php
+define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
+define('BASE_URL', 'http://localhost:5500/');
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+require './config/variables.php';
+require ROOT_PATH . '/config/database.php';
