7e2f88bd08da5e8d65b55f557dbeb33aee64451d
Sorann753 <Arthusd@hotmail.fr>
2022-11-10 23:25:12 +0100

    moved favicon to assets folder
    - moved favicon to assets folder
    - added getBdd function to create PDO object

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <header class="mb-auto">
     <nav class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-            <img src="./favicon_io/favicon-32x32.png" alt="Logo" />
+            <img src="./assets/favicon_io/favicon-32x32.png" alt="Logo" />
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
