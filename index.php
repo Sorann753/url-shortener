875c0590d8b6bdaa28865d0d5eb267ec5b98d45d
Sorann753 <Arthusd@hotmail.fr>
2022-11-11 01:41:42 +0100

    created router in index.php
    - index.php is now a router
    - moved footer, head and header to views/includes/
    - created home_view.php and home_controller.php
    - update database diagram

--- index.php
+++ index.php
@@ -1,34 +1,31 @@
 <?php
+session_start();
+
 require_once './config/const.php';
 require ROOT_PATH . '/models/bdd.php';
 require ROOT_PATH . '/includes/utils.php';
 
 $bdd = new Bdd();
-?>
 
-<!DOCTYPE html>
-<html lang="en" class="h-100 w-100">
+$page = filter_input(INPUT_GET, 'page');
+$shortUrl = filter_input(INPUT_GET, 'url');
 
-<?php require_once ROOT_PATH . '/includes/head.php' ?>
+if($url){
+    $trueUrl = $bdd.getUrlByShortUrl($shortUrl);
+    header('Location: ' . $trueUrl);
+}
 
-<body class="d-flex h-100 text-center text-bg-dark spa">
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-        <!-- HEADER -->
-        <?php require ROOT_PATH . '/includes/header.php'; ?>
+if (! $page) {
+    $page = 'home';
+}
 
-        <!-- CONTENT -->
-        <main class="px-3">
-            <h1>Welcome to Url Shortener !</h1>
-            <p class="lead">It's very simple ! You want a preview ? No problem ! Try it below !</p>
-            <form class="mx-auto d-flex px-4">
-                <input type="text" class="form-control form-control-dark text-bg-dark text-center fs-4" placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ" aria-label="https://www.youtube.com/watch?v=dQw4w9WgXcQ" name="url" id="url">
-                <input type="submit" class="btn btn-lg btn-secondary text-black fw-bold border-white bg-white ms-3" value="Short URL">
-            </form>
-        </main>
+$allPages = scandir(ROOT_PATH . '/controllers/');
 
-        <!-- FOOTER -->
-        <?php require_once ROOT_PATH . '/includes/footer.php'; ?>
-    </div>
-</body>
-
-</html>
+if (in_array($page . '_controller.php', $allPages)) {
+    require (ROOT_PATH . '/controllers/' . $page . '_controller.php');
+    require (ROOT_PATH . '/views/' . $page . '_view.php');
+}
+else {
+    header("Location: /index.php?page=home");
+    exit();
+}
