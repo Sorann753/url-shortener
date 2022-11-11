a2e5d21a101f2d50bf2c2b646076456c78371f40
Snowsdy <shadowss.yt@gmail.com>
2022-11-11 22:36:02 +0100

    ✨ Fix for MAMP check

--- config/const.php
+++ config/const.php
@@ -1,3 +1,8 @@
 <?php
+$server = $_SERVER['SERVER_SOFTWARE'];
+if (strpos($server, 'nginx')) {
+    define('BASE_URL', $_SERVER['SERVER_NAME'] . '/url-shortener');
+} else {
+    define('BASE_URL', '/url-shortener');
+}
 define('ROOT_PATH', realpath($_SERVER['DOCUMENT_ROOT'] . '/url-shortener'));
-define('BASE_URL', $_SERVER['SERVER_NAME'] . '/url-shortener');
