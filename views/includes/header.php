82c412ad7ce8b6a2519510ceddcfe742de7d978c
Snowsdy <shadowss.yt@gmail.com>
2022-11-11 05:23:09 +0100

    🐛 In english please

--- views/includes/header.php
+++ views/includes/header.php
@@ -11,7 +11,7 @@
 
         <div class="text-end d-flex flex-wrap align-items-center justify-content-center justify-content-lg-end">
             <?php if (userConnected()) : ?>
-                <p class="lead me-4 pt-2">Bienvenue <strong><?= $_SESSION['user'] ?></strong></p>
+                <p class="lead me-4 pt-2">Welcome <strong><?= $_SESSION['user'] ?></strong></p>
                 <a href="index?page=logout" class="btn btn-danger me-2">Logout</a>
             <?php else : ?>
                 <a href="/index.php?page=login" class="btn btn-outline-light me-2">Login</a>
