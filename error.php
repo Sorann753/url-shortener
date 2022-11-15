a6d2310938af30b7bf2baa2c7b5a03356b01fa5a
Snowsdy <shadowss.yt@gmail.com>
2022-11-15 23:42:38 +0100

    Error Style :)

--- error.php
+++ error.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
 <html lang="en" class="h-100 w-100">
 
-<?php require_once ROOT_PATH . '/views/includes/head.php' ?>
+<?php require_once './views/includes/head.php' ?>
 
 <body class="d-flex h-100 text-center text-bg-dark">
-    <h1><span class="badge badge-danger">Welp, everything is dead, please come back later</span></h1>
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <main class="px-3">
+            <h1><span class="badge text-bg-danger">Welp, everything is dead, please come back later</span></h1>
+            <img src="./assets/imgs/error.gif" class="rounded mx-auto d-block my-4 h-75 w-75" alt="Error image">
+        </main>
 
-    <p class="alert alert-primary" role="alert">
-        <?= $error ?>
-    </p>
-    <!-- FOOTER -->
-    <?php require_once ROOT_PATH . '/views/includes/footer.php'; ?>
+        <footer class="mt-auto">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item"><a href="index.php?page=home" class="nav-link px-2 text-muted">Home</a></li>
+            </ul>
+            <p class="text-center text-muted">© 2022 Company, Inc</p>
+        </footer>
+    </div>
 </body>
+
 
 </html>
